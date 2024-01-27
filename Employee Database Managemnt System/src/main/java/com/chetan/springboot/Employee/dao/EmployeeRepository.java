29d61db622631e2573dbfc2d4679b7d1c0d6d38c
Chetan090302 <134623805+Chetan090302@users.noreply.github.com>
2024-01-27 12:44:41 +0530

    Add files via upload

---
+++ Employee Database Managemnt System/src/main/java/com/chetan/springboot/Employee/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.chetan.springboot.Employee.dao;
+
+import com.chetan.springboot.Employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
+
---
+++ Employee Database Managemnt System/src/main/java/com/chetan/springboot/Employee/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.chetan.springboot.Employee.service;
+
+
+
+import com.chetan.springboot.Employee.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
+
